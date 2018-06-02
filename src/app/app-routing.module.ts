76601327cb5485da9f13b4242b59fc8a90564c65
Stephane Eybert <mittiprovence@yahoo.se>
2018-06-02 16:04:48 +0200

    Formatted the code.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,20 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-import {PreloadAllModules} from '@angular/router';
+import { PreloadAllModules } from '@angular/router';
 // See more on preloading https://alligator.io/angular/preloading/
 
-import {AuthGuardService} from './auth-guard.service';
-import {HeroesComponent} from './heroes/heroes.component';
-import {DashboardComponent} from './dashboard/dashboard.component';
-import {HeroDetailComponent} from './hero-detail/hero-detail.component';
-import {LoginComponent} from './login/login.component';
+import { AuthGuardService } from './auth-guard.service';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-  {path: 'login', component: LoginComponent},
-  {path: 'detail/:id', component: HeroDetailComponent, canActivate: [AuthGuardService]},
-  {path: 'heroes', component: HeroesComponent, canActivate: [AuthGuardService]},
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'detail/:id', component: HeroDetailComponent, canActivate: [AuthGuardService] },
+  { path: 'heroes', component: HeroesComponent, canActivate: [AuthGuardService] },
   {
     path: 'dashboard',
     component: DashboardComponent,
@@ -23,11 +23,11 @@ const routes: Routes = [
       expectedRole: 'admin'
     }
   },
-  {path: '**', redirectTo: ''}
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
