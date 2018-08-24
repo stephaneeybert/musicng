944960631e00c77443308615ab6353d4aadf4d16
Stephane Eybert <mittiprovence@yahoo.se>
2018-08-24 13:08:55 +0200

    Trying to have the request retry on server errors only and display the original error in the custom error handler.

--- src/app/core/error/error-request-interceptor.ts
+++ src/app/core/error/error-request-interceptor.ts
@@ -9,16 +9,21 @@ import {
 } from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/retry';
+import 'rxjs/add/operator/retryWhen';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/concat';
 
 import { ErrorCustomHandler } from './error-custom-handler';
+import { catchError } from 'rxjs/operators';
 
 // Because the best Error is the one that never happens, improve the error handling
 // using an HttpInterceptor to intercept all the server calls and retry them n times
 // before throwing an error
 const NB_RETRIES = 3;
-const SERVER_ERROR = /^5.*$/;
+const HTTP_SERVER_ERROR = /^5.*$/;
 
 @Injectable()
 export class ErrorRequestInterceptor implements HttpInterceptor {
@@ -28,15 +33,29 @@ export class ErrorRequestInterceptor implements HttpInterceptor {
     ) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(request).retry(NB_RETRIES).do((event: HttpEvent<any>) => { }, (error: any) => {
-            const isServerError: boolean = this.isServerError(error);
-            if (error instanceof HttpErrorResponse && isServerError) {
-                this.errorCustomHandler.handleError(error);
-            }
+        return next.handle(request).retryWhen(errors => {
+            return errors.switchMap(error => {
+                if (this.isServerError(error)) {
+                    return Observable.of(error).delay(200);
+                } else {
+                    return Observable.throw(error);
+                }
+            })
+            .take(NB_RETRIES)
+            .concat(Observable.throw({error: 'There was a server error (after ' + NB_RETRIES + ' retries)'}));
         });
     }
+    // TODO https://stackoverflow.com/questions/51999929
+    // intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    //     return next.handle(request).retry(NB_RETRIES).do((event: HttpEvent<any>) => { }, (error: any) => {
+    //         const isServerError: boolean = this.isServerError(error);
+    //         if (error instanceof HttpErrorResponse && isServerError) {
+    //             this.errorCustomHandler.handleError(error);
+    //         }
+    //     });
+    // }
 
     private isServerError(error): boolean {
-        return SERVER_ERROR.test((error.status.toString()));
+        return HTTP_SERVER_ERROR.test((error.status.toString()));
     }
 }
