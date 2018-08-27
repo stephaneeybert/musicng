2409b66a3e92b8b4c06889a2475c1b3e88e3076d
Stephane Eybert <mittiprovence@yahoo.se>
2018-08-27 13:04:57 +0200

    Ordered the imports by type.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+import { ErrorModule } from './core/error';
 import { AppGuiModule } from './app-gui.module';
 import { AuthModule } from './core/auth/auth.module';
+
+import { MessageService } from './core/messages/message.service';
+import { HttpService } from './core/service/http.service';
+import { NotificationService } from './core/service/notification.service';
+import { UserService } from './modules/user/user.service';
+import { KeycloakClientService } from './core/auth/keycloak-client.service';
+
 import { AppComponent } from './app.component';
 import { MessagesComponent } from './core/messages/messages.component';
-import { MessageService } from './core/messages/message.service';
 import { LoginComponent } from './core/login/login.component';
 import { UsersComponent } from './modules/user/users.component';
 import { UserDetailComponent } from './modules/user/user.component';
-import { UserService } from './modules/user/user.service';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import { UserSearchComponent } from './modules/user/search.component';
 
-import { NotificationService } from './core/service/notification.service';
-import { HttpService } from './core/service/http.service';
-import { ErrorModule } from './core/error';
-import { KeycloakClientService } from './core/auth/keycloak-client.service';
 
 @NgModule({
   declarations: [
