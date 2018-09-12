fb2cdf91a836111cd850a0810857a3cd8662be53
Stephane Eybert <mittiprovence@yahoo.se>
2018-09-12 16:00:16 +0200

    Added support for url parameters in the GET request.

--- src/app/core/service/http.service.ts
+++ src/app/core/service/http.service.ts
@@ -1,51 +1,57 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { HttpClient, HttpHeaders, HttpEvent } from '@angular/common/http';
-import { HttpParamsOptions } from '@angular/common/http/src/params';
+import { HttpClient, HttpParams, HttpHeaders, HttpEvent } from '@angular/common/http';
 
 @Injectable()
 export class HttpService {
 
     constructor(private httpClient: HttpClient) { }
 
-    public get<T>(url: string, headers?: HttpHeaders | null): Observable<T> {
-        return this.httpClient.get<T>(url, this.buildOptionsForResponseBodyOnly(headers));
+    public get<T>(url: string, httpParams?: HttpParams | null, headers?: HttpHeaders | null): Observable<T> {
+        let options = this.buildOptions(headers);
+        options = this.addOptionParams(options, httpParams);
+        return this.httpClient.get<T>(url, options);
     }
 
     public post<T>(url: string, body: object, headers?: HttpHeaders | null): Observable<T> {
-        return this.httpClient.post<T>(url, body, this.buildOptionsForResponseBodyOnly(headers));
+        return this.httpClient.post<T>(url, body, this.buildOptions(headers));
     }
 
     public postWithHeadersInResponse<T>(url: string, body: object, headers?: HttpHeaders | null): Observable<T> {
-        return this.httpClient.post<T>(url, body, this.buildOptionsForCompleteResponse(headers));
+        let options = this.buildOptions(headers);
+        options = this.addOptionForCompleteResponse(options);
+        return this.httpClient.post<T>(url, body, options);
     }
 
     public put<T>(url: string, body: object, headers?: HttpHeaders | null): Observable<T> {
-        return this.httpClient.put<T>(url, body, this.buildOptionsForResponseBodyOnly(headers));
+        return this.httpClient.put<T>(url, body, this.buildOptions(headers));
     }
 
     public patch<T>(url: string, body: object, headers?: HttpHeaders | null): Observable<T> {
-        return this.httpClient.patch<T>(url, body, this.buildOptionsForResponseBodyOnly(headers));
+        return this.httpClient.patch<T>(url, body, this.buildOptions(headers));
     }
 
     public delete<T>(url: string, headers?: HttpHeaders | null): Observable<T> {
-        return this.httpClient.delete<T>(url, this.buildOptionsForResponseBodyOnly(headers));
+        return this.httpClient.delete<T>(url, this.buildOptions(headers));
     }
 
-    private buildOptionsForCompleteResponse(headers: HttpHeaders) {
-        return {
-            headers: this.buildHeader(headers),
-            // Have the response headers included in the response object
-            observe: 'response' as 'body',
-            responseType: 'json' as 'json'
-        };
-    }
-
-    private buildOptionsForResponseBodyOnly(headers: HttpHeaders) {
-        return {
+    private buildOptions(headers: HttpHeaders) {
+        const options = {
             headers: this.buildHeader(headers),
             responseType: 'json' as 'json'
         };
+        return options;
+    }
+
+    private addOptionParams(options, httpParams) {
+        options['params'] = httpParams;
+        return options;
+    }
+
+    // Have the response headers included in the response object
+    private addOptionForCompleteResponse(options) {
+        options['observe'] = 'response' as 'body';
+        return options;
     }
 
     public buildHeader(headers: HttpHeaders | null): HttpHeaders {
