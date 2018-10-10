a8f660ef6844ffc77b0936e297f8d08dad5923fd
Stephane Eybert <mittiprovence@yahoo.se>
2018-10-10 15:14:34 +0200

    Added a separating blank line.

--- src/app/app-gui.module.ts
+++ src/app/app-gui.module.ts
@@ -3,6 +3,7 @@ import { MatDialogModule, MatDialog } from '@angular/material/dialog';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule, MatInputModule } from '@angular/material';
+
 import { LoginDialogComponent } from './core/login/login-dialog.component';
 
 @NgModule({
