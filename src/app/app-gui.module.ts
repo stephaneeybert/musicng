fb94810fc28de746656e0622f913223f4f52c1d4
Stephane Eybert <mittiprovence@yahoo.se>
2018-10-10 15:16:59 +0200

    Ordered import statements.

--- src/app/app-gui.module.ts
+++ src/app/app-gui.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
-import { MatDialogModule, MatDialog } from '@angular/material/dialog';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MatDialogModule, MatDialog } from '@angular/material/dialog';
 
 import { LoginDialogComponent } from './core/login/login-dialog.component';
 
