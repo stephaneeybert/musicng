ce5aa68e15d147d5f5f879a9d5f6ae093e00d658
Stephane Eybert <mittiprovence@yahoo.se>
2018-10-10 23:24:03 +0200

    Ordered the imported elements.

--- src/app/core/login/login-dialog.component.ts
+++ src/app/core/login/login-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialogRef, MatDialog, MAT_DIALOG_DATA } from '@angular/material';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { FormGroup, FormBuilder } from '@angular/forms';
 
 
