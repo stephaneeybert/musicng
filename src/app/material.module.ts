90abc04c2229a867d26594b015a18a2a9f3322ca
Stephane Eybert <mittiprovence@yahoo.se>
2018-10-11 15:46:32 +0200

    Grouped an import with the others of the same kind.

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
 import {
-  MatButtonModule, MatSidenavModule, MatIconModule,
+  MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule,
   MatListModule, MatGridListModule, MatCardModule, MatMenuModule,
   MatTableModule, MatPaginatorModule, MatSortModule,
   MatDialogModule,
