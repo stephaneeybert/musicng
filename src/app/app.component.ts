cc0186cd20e1da345fe749ba245efb454e02efd6
Stephane Eybert <mittiprovence@yahoo.se>
2018-10-13 16:07:03 +0200

    Added a user message offering him to get the latest version of the application if this one had changed and he had not yet reloaded the web page to get it.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'Tour of Zeroes';
+
+  constructor(private swUpdate: SwUpdate) {
+  }
+
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if (confirm('A newer version of the application is available. Load the new version ?')) {
+          window.location.reload();
+        }
+      });
+    }
+  }
+
 }
