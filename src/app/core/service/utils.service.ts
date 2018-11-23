ba043ddaed52d006e5b4b212245f32fe517f23b9
Stephane Eybert <mittiprovence@yahoo.se>
2018-11-23 12:43:22 +0100

    Added a TODO comment.

--- src/app/core/service/utils.service.ts
+++ src/app/core/service/utils.service.ts
@@ -2,6 +2,11 @@ import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material';
 
 @Injectable()
+// TODO The only notable thing is that we use the new providedIn attribute of the Injectable decorator that’s available since Angular 6, specifying ‘root’ as a value. This means that this service will be registered, as a singleton, in our application and that we don’t need to add it to the providers array anymore.
+// This actually means that our services are now tree-shakeable. If we have a service defined in our application, but it’s not referenced in any other place, it will be removed from the final bundle. This was not the case when we had to add them to the providers array of a module. Angular itself is starting to move its built-in services to this way of registering them and I suggest you start doing it as well.
+// @Injectable({
+//     providedIn: 'root'
+// })
 export class UtilsService {
 
     constructor(
