916c210f3a139382f25b1eef1de7a92189b35165
Stephane Eybert <mittiprovence@yahoo.se>
2019-01-25 14:19:29 +0200

    Corrected the production environment hostname.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   CLIENT_ID: 'NG-ZERO',
   ROLE_PREFIX: 'role_',
-  BASE_REST_URI: 'http://localhost:8080/api',
-  KEYCLOAK_URI: 'http://localhost:8180/auth',
+  BASE_REST_URI: 'http://thalasoft.com:8080/api',
+  KEYCLOAK_URI: 'http://thalasoft.com:8180/auth',
   KEYCLOAK_REALM: 'learnintouch',
   KEYCLOAK_CLIENTID: 'learnintouch-js'
 };
