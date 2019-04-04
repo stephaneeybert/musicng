ba1283204d76dd8a610ff0afdab46165aba5d3c3
Stephane Eybert <mittiprovence@yahoo.se>
2019-04-04 12:03:30 +0300

    Transformed a template form into a reactive form.

--- src/app/views/user/user.component.ts
+++ src/app/views/user/user.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
 import { UserService } from '@app/views/user/user.service';
 import { User } from '@app/views/user/user';
@@ -12,12 +12,16 @@ import { User } from '@app/views/user/user';
 })
 export class UserComponent implements OnInit {
 
-  @Input() user: User;
+  user: User;
+
+  form: FormGroup;
+  private formSubmitAttempt: boolean;
 
   constructor(
+    private formBuilder: FormBuilder,
     private route: ActivatedRoute,
+    private router: Router,
     private userService: UserService,
-    private location: Location
   ) { }
 
   ngOnInit(): void {
@@ -27,20 +31,33 @@ export class UserComponent implements OnInit {
   getUser(): void {
     const id = +this.route.snapshot.paramMap.get('id');
     this.userService.get(id)
-      .subscribe(user => {
-        this.user = user;
+    .subscribe(user => {
+      this.user = user;
+      this.form = this.formBuilder.group({
+        firstname: [this.user.firstname, Validators.required],
+        lastname: [this.user.lastname, Validators.required]
       });
+    });
   }
 
-  goBack(): void {
-    this.location.back();
+  isFieldInvalid(field: string): boolean {
+    return (
+      (!this.form.get(field).valid && this.form.get(field).touched) ||
+      (this.form.get(field).untouched && this.formSubmitAttempt)
+    );
   }
 
   save(): void {
+    this.user.firstname = this.form.get('firstname').value;
+    this.user.lastname = this.form.get('lastname').value;
     this.userService.partialUpdate(this.user)
       .subscribe(() => {
-        this.goBack();
+        this.router.navigate(['users']);
       });
+  }
+
+  cancel(): void {
+    this.router.navigate(['users']);
   }
 
 }
