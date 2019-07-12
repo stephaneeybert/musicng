d54d69f82adaf82db0928881a91d11dccbb407c9
Stephane Eybert <mittiprovence@yahoo.se>
2019-07-12 15:06:17 +0200

    Removed a redundant slash character.

--- src/app/core/error/index.ts
+++ src/app/core/error/index.ts
@@ -1,4 +1,4 @@
 export { ErrorModule } from './error.module';
 export { ErrorService } from './error.service';
 export { ErrorCustomHandler } from './error-custom-handler';
-export { ErrorComponent } from './/error.component';
+export { ErrorComponent } from './error.component';
