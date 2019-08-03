378499a489f966f9a4e30d548e82b369918c3085
Stephane Eybert <mittiprovence@yahoo.se>
2019-08-03 16:06:04 +0200

    Made the service injectable.

--- src/app/core/service/ui.service.ts
+++ src/app/core/service/ui.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root',
+})
 export class UiService {
 
   private appColor: '#343a40';
