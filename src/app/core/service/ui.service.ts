0cd1bb7f46a26a9f692e644a0bb3fce9677aa9b2
Stephane Eybert <mittiprovence@yahoo.se>
2019-08-03 16:27:58 +0200

    Removed an unneeded trailing comma;

--- src/app/core/service/ui.service.ts
+++ src/app/core/service/ui.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class UiService {
 
