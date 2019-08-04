96c48d715d6bb9bd96ce59c67bb56d98ebaddeed
Stephane Eybert <mittiprovence@yahoo.se>
2019-08-04 09:23:02 +0200

    Formatted the entire source code.

--- src/app/core/error/error.service.ts
+++ src/app/core/error/error.service.ts
@@ -11,62 +11,62 @@ import * as StackTraceParser from 'error-stack-parser';
 @Injectable()
 export class ErrorService {
 
-    constructor(
-        private injector: Injector,
-        private router: Router,
-        private httpClient: HttpClient,
-    ) {
-        // Subscribe to the navigation errors
-        this.router
-            .events
-            .subscribe((event: Event) => {
-                if (event instanceof NavigationError) {
-                    this.log(event.error)
-                        .subscribe((errorWithContext) => {
-                            this.router.navigate(['error'], { queryParams: errorWithContext });
-                        });
-                }
+  constructor(
+    private injector: Injector,
+    private router: Router,
+    private httpClient: HttpClient,
+  ) {
+    // Subscribe to the navigation errors
+    this.router
+      .events
+      .subscribe((event: Event) => {
+        if (event instanceof NavigationError) {
+          this.log(event.error)
+            .subscribe((errorWithContext) => {
+              this.router.navigate(['error'], { queryParams: errorWithContext });
             });
-    }
+        }
+      });
+  }
 
-    public log(error) {
-        const errorWithContext = this.addContextInfo(error);
-        return MockHttpService.post(errorWithContext); // TODO Implement a server side error inbox
-    }
+  public log(error) {
+    const errorWithContext = this.addContextInfo(error);
+    return MockHttpService.post(errorWithContext); // TODO Implement a server side error inbox
+  }
 
-    private addContextInfo(error) {
-        const appId = 'My API id'; // TODO get an env variable
-        const location = this.injector.get(LocationStrategy);
-        const url = location instanceof PathLocationStrategy ? location.path() : '';
-        const time = new Date().getTime();
+  private addContextInfo(error) {
+    const appId = 'My API id'; // TODO get an env variable
+    const location = this.injector.get(LocationStrategy);
+    const url = location instanceof PathLocationStrategy ? location.path() : '';
+    const time = new Date().getTime();
 
-        const name = error.name || null;
-        const user = ''; // TODO get the logged in user
-        const id = `${appId}-${user}-${time}`;
-        const status = error.status || null;
-        const message = (error && error.body) ? error.body.error : error.toString();
-        const stack = error instanceof HttpErrorResponse ? null : error; // TODO StackTraceParser.parse(error);
-        const method = (stack && stack[0]) ? stack[0].functionName : null;
+    const name = error.name || null;
+    const user = ''; // TODO get the logged in user
+    const id = `${appId}-${user}-${time}`;
+    const status = error.status || null;
+    const message = (error && error.body) ? error.body.error : error.toString();
+    const stack = error instanceof HttpErrorResponse ? null : error; // TODO StackTraceParser.parse(error);
+    const method = (stack && stack[0]) ? stack[0].functionName : null;
 
-        const errorWithContext = { message, method, name, appId, user, time, id, url, status, stack };
-        return errorWithContext;
-    }
+    const errorWithContext = { message, method, name, appId, user, time, id, url, status, stack };
+    return errorWithContext;
+  }
 
-    fireFakeClientError() {
-        throw new Error('Another runtime error)');
-        // As the 'it' object is not defined, this should produce a runtime error
-        // return it.happens;
-    }
+  fireFakeClientError() {
+    throw new Error('Another runtime error)');
+    // As the 'it' object is not defined, this should produce a runtime error
+    // return it.happens;
+  }
 
-    fireFakeServerError() {
-        this.httpClient
-            .get('https://jsonplaceholder.typicode.com/1')
-            .subscribe();
-    }
+  fireFakeServerError() {
+    this.httpClient
+      .get('https://jsonplaceholder.typicode.com/1')
+      .subscribe();
+  }
 }
 
 class MockHttpService {
-    static post(error): Observable<any> {
-        return of(error);
-    }
+  static post(error): Observable<any> {
+    return of(error);
+  }
 }
