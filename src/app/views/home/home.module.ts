0072f0f841a2ce405dd47226f062bc970dcb473c
Stephane Eybert <mittiprovence@yahoo.se>
2019-11-24 00:10:03 +0200

    Imported the external midi-lib library.

--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { MaterialModule } from '@app/material.module';
-import { MidiLibModule } from '@lib/midi-lib.module';
+import { MidiLibModule } from 'midi-lib';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
