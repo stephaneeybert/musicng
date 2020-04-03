02e621e2811c189b82997e78f32aa1dc3dfcfdbc
Stephane Eybert <mittiprovence@yahoo.se>
2020-04-03 16:48:16 +0200

    Added allowed characters for the soundtrack name.

--- src/app/views/soundtrack/validate-soundtrack-name.ts
+++ src/app/views/soundtrack/validate-soundtrack-name.ts
@@ -1,6 +1,6 @@
 import { AbstractControl } from '@angular/forms';
 
-const REGEX_ALLOWED_CHARS: any = /^[a-z0-9]+$/i;
+const REGEX_ALLOWED_CHARS: any = /^[a-z0-9_-]+$/i;
 
 export function ValidateSoundtrackName(control: AbstractControl) {
   if (!REGEX_ALLOWED_CHARS.test(control.value)) {
