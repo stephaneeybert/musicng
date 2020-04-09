8f30b4214dbf102c0d655e7d413e94cab9a880e0
Stephane Eybert <mittiprovence@yahoo.se>
2020-04-09 14:37:49 +0200

    Beautified the source code.

--- src/app/core/service/utils.service.ts
+++ src/app/core/service/utils.service.ts
@@ -2,17 +2,17 @@ import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class UtilsService {
 
-    constructor(
-        private matSnackBar: MatSnackBar
-    ) { }
+  constructor(
+    private matSnackBar: MatSnackBar
+  ) { }
 
-    public showSnackBar(message: string, action: string): void {
-        this.matSnackBar.open(message, action, {
-            duration: 2000,
-        });
-    }
+  public showSnackBar(message: string, action: string): void {
+    this.matSnackBar.open(message, action, {
+      duration: 2000,
+    });
+  }
 }
