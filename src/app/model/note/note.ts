369eaabf63b14d112b53c730e89dfbf40ce0e6c6
Stephane Eybert <mittiprovence@yahoo.se>
2020-04-14 09:23:16 +0200

    Added a TODO comment.

--- src/app/model/note/note.ts
+++ src/app/model/note/note.ts
@@ -7,7 +7,7 @@ export class Note {
 
   // TODO note.probability = 0.5; // Events have a probability parameter which allows you to adjust the probability of the event firing each time it is scheduled to.
   // TODO note.humanize = "32n"; // "Humanization" let's you adjust how rigid the callback timing is. If humanize is set to true, the passed-in time parameter will drift back and forth slightly to make the part feel a little more "human". You can also set humanize to a Time value, which will make it drift by that amount.
-  index: number;
+  index: number; // TODO The index is still not used
   pitch: Pitch;
   velocity?: number;
   dotted: boolean;
