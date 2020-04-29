5faf7b118fcacfd7d4542e084c9cc8e418ee4f52
Stephane Eybert <mittiprovence@yahoo.se>
2020-04-29 10:46:31 +0200

    Added a missing unsubscrie to the observable subscription.

--- src/app/layouts/unsecured/unsecured.sidenav.component.ts
+++ src/app/layouts/unsecured/unsecured.sidenav.component.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-unsecured-sidenav',
   templateUrl: './unsecured.sidenav.component.html'
 })
-export class UnsecuredSidenavComponent implements OnInit {
+export class UnsecuredSidenavComponent implements OnInit, OnDestroy {
+
+  subscription: Subscription;
 
   constructor(
     private translateService: TranslateService
-  ) { }
+  ) {
+    this.subscription = new Subscription();
+  }
 
   public ngOnInit() {
-    this.translateService.get('app.title').subscribe((text: string) => {
-      console.log('The app title: ' + text);
-    });
+    this.subscription.add(
+      this.translateService.get('app.title').subscribe((text: string) => { // TODO Missing unsubscribe
+        console.log('The app title: ' + text);
+      })
+    );
+  }
+
+  public ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
