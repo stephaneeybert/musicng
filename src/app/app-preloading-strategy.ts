d76bbd33f2638cf841ca31aee7607db22e74670f
Stephane Eybert <mittiprovence@yahoo.se>
2020-05-01 15:09:42 +0200

    Rewrote a method with a more explicit and readable syntax.

--- src/app/app-preloading-strategy.ts
+++ src/app/app-preloading-strategy.ts
@@ -6,12 +6,13 @@ import { mergeMap } from 'rxjs/operators';
 export class AppPreloadingStrategy implements PreloadingStrategy {
 
     preload(route: Route, load: Function): Observable<any> {
-        const loadRoute = (delay: number) => delay
-            ? timer(150).pipe(mergeMap(_ => load()))
-            : load();
-        return route.data && route.data.preload
-            ? loadRoute(route.data.delay)
-            : of(null);
+      return route.data && route.data.preload
+      ? this.loadRoute(route.data.delay, load)
+      : of(null);
+    }
+
+    private loadRoute(delay: number, load: Function): Observable<any> {
+      return delay ? timer(150).pipe(mergeMap(_ => load())) : load();
     }
 
 }
