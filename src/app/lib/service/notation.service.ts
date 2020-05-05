c5e13118906fba5138be7c4f0aeaa2016bc60170
Stephane Eybert <mittiprovence@yahoo.se>
2020-05-05 14:12:48 +0200

    Corrected the syllabic chromas.

--- src/app/lib/service/notation.service.ts
+++ src/app/lib/service/notation.service.ts
@@ -28,7 +28,7 @@ const DEFAULT_TIME_SIGNATURE_DENOMINATOR: number = 4;
 const VELOCITY_MIDI_MAX: number = 127;
 const CHROMAS_ALPHABETICAL: Array<string> = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 const CHROMAS_GERMAN_ALPHABETICAL: Array<string> = ['C', 'D', 'E', 'F', 'G', 'A', 'H'];
-const CHROMAS_SYLLABIC: Map<string, string> = new Map([ ['rest', 'rest'], ['C', 'Do'], ['C#', '???'], ['D', 'Re.m'], ['D#', '???'], ['E', 'Mi.m'], ['F', 'Fa'], ['F#', '???'], ['G', 'Sol'], ['G#', '???'], ['A', 'La.m'], ['A#', '???'], ['B', 'Si-'] ]); // TODO Replace the ???
+const CHROMAS_SYLLABIC: Map<string, string> = new Map([ ['rest', 'rest'], ['C', 'Do'], ['C#', 'Do#'], ['D', 'Re'], ['D#', 'Re#'], ['E', 'Mi'], ['F', 'Fa'], ['F#', 'Fa#'], ['G', 'Sol'], ['G#', 'Sol#'], ['A', 'La'], ['A#', 'La#'], ['B', 'Si'] ]);
 const OCTAVES: Array<number> = [ 1, 2, 3, 4, 5, 6 ];
 
 @Injectable({
