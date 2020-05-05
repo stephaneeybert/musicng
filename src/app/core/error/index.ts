c797ac6e9b69d505801461f81a7458c762d8a6fc
Stephane Eybert <mittiprovence@yahoo.se>
2020-05-05 14:46:18 +0200

    Removed two uselessindex files.

--- src/app/core/error/index.ts
+++
@@ -1,4 +0,0 @@
-export { ErrorModule } from './error.module';
-export { ErrorService } from './error.service';
-export { ErrorCustomHandler } from './error-custom-handler';
-export { ErrorComponent } from './error.component';
