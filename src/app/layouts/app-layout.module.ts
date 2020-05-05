d656affb9341c6be256ef598375575e75864eeb2
Stephane Eybert <mittiprovence@yahoo.se>
2020-05-05 11:25:26 +0200

    Replaced the local pwa by the newly created library.

--- src/app/layouts/app-layout.module.ts
+++ src/app/layouts/app-layout.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from '@app/app-routing.module';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MaterialModule } from '@app/material.module';
@@ -9,12 +8,12 @@ import { SecuredLayoutComponent } from '@app/layouts/secured/secured.layout.comp
 import { HeaderComponent } from '@app/layouts/unsecured/header.component';
 import { SecuredSidenavComponent } from '@app/layouts/secured/secured.sidenav.component';
 import { UnsecuredSidenavComponent } from '@app/layouts/unsecured/unsecured.sidenav.component';
-import { LibI18nModule } from 'lib-i18n';
-import { LibCoreModule } from 'lib-core';
 import { SettingsComponent } from '@app/views/settings/settings.component';
 import { AppUiModule } from '@app/app-ui.module';
 import { SettingsDialogComponent } from '@app/views/settings/settings-dialog.component';
-import { PwaModule } from '@app/core/pwa/pwa.module';
+import { LibI18nModule } from 'lib-i18n';
+import { LibCoreModule } from 'lib-core';
+import { LibPwaModule } from 'lib-pwa';
 
 @NgModule({
   declarations: [
@@ -28,26 +27,23 @@ import { PwaModule } from '@app/core/pwa/pwa.module';
   ],
   imports: [
     AppRoutingModule,
-    PwaModule,
     LayoutModule,
     AppUiModule,
+    MaterialModule,
+    MDBootstrapModule,
     LibI18nModule,
     LibCoreModule,
-    MaterialModule,
-    MDBootstrapModule
+    LibPwaModule
   ],
   exports: [
     AppRoutingModule,
-    PwaModule,
     LayoutModule,
     AppUiModule,
+    MaterialModule,
+    MDBootstrapModule,
     LibI18nModule,
     LibCoreModule,
-    MaterialModule,
-    MDBootstrapModule
-  ],
-  // entryComponents: [ // TODO Not needed ?
-  //   SettingsDialogComponent
-  // ]
+    LibPwaModule
+  ]
 })
 export class AppLayoutModule { }
