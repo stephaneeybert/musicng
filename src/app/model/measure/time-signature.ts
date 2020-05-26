83f13b3754ed903de2532ea4c02523e0812d8fff
Stephane Eybert <mittiprovence@yahoo.se>
2020-05-26 15:53:48 +0200

    Added some examples in the comment

--- src/app/model/measure/time-signature.ts
+++ src/app/model/measure/time-signature.ts
@@ -2,7 +2,7 @@ export class TimeSignature {
 
   // The number of beats in a measure
   numerator: number;
-  // The type of note that gets one beat
+  // The type of note (1 = whole, 2 = half, 4 = quarter) that gets one beat
   denominator: number;
 
   constructor(numerator: number, denominator: number) {
