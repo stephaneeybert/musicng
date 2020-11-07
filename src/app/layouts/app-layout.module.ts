b186475fdda31a51bc68206ff5b0e5b8bb0ea775
Stephane Eybert <mittiprovence@yahoo.se>
2020-11-07 18:36:47 +0100

    Split the header toolbar  in two rows.

--- src/app/layouts/app-layout.module.ts
+++ src/app/layouts/app-layout.module.ts
@@ -6,7 +6,6 @@ import { SensorModule } from '@app/core/sensor/sensor.module';
 import { ThemeModule } from '@app/core/theme/theme.module';
 import { SecuredLayoutComponent } from '@app/layouts/secured/secured.layout.component';
 import { SecuredSidenavComponent } from '@app/layouts/secured/secured.sidenav.component';
-import { HeaderComponent } from '@app/layouts/unsecured/header.component';
 import { UnsecuredLayoutComponent } from '@app/layouts/unsecured/unsecured.layout.component';
 import { UnsecuredSidenavComponent } from '@app/layouts/unsecured/unsecured.sidenav.component';
 import { MaterialModule } from '@app/material.module';
@@ -23,7 +22,6 @@ import { LibPwaModule } from '@stephaneeybert/lib-pwa';
   declarations: [
     SecuredLayoutComponent,
     UnsecuredLayoutComponent,
-    HeaderComponent,
     SecuredSidenavComponent,
     UnsecuredSidenavComponent,
     SettingsDialogComponent,
